68398c034e676e965dc4a66649f0a3998a6de628
kscript <1258@live.cn>
2021-06-21 12:45:22 +0800

    添加子菜单

--- src/docs/snippet/index.ts
+++ src/docs/snippet/index.ts
@@ -1,23 +1,28 @@
-const list: Menu[] = [
+const list: MenuList = [
   {
-    title: 'CSS省略号',
-    path: 'CSS省略号.md',
-    content: require('./CSS省略号.md').default.render()
-  },
-  {
-    title: 'CSS夜间主题',
-    path: 'CSS夜间主题.md',
-    content: require('./CSS夜间主题.md').default.render()
-  },
-  {
-    title: 'CSS灰色效果',
-    path: 'CSS灰色效果.md',
-    content: require('./CSS灰色效果.md').default.render()
-  },
-  {
-    title: 'CSS过渡效果导致元素抖动',
-    path: 'CSS过渡效果导致元素抖动.md',
-    content: require('./CSS过渡效果导致元素抖动.md').default.render()
+    title: 'CSS',
+    menu: [
+      {
+        title: 'CSS省略号',
+        path: 'CSS省略号.md',
+        content: require('./CSS省略号.md').default.render()
+      },
+      {
+        title: 'CSS夜间主题',
+        path: 'CSS夜间主题.md',
+        content: require('./CSS夜间主题.md').default.render()
+      },
+      {
+        title: 'CSS灰色效果',
+        path: 'CSS灰色效果.md',
+        content: require('./CSS灰色效果.md').default.render()
+      },
+      {
+        title: 'CSS过渡效果导致元素抖动',
+        path: 'CSS过渡效果导致元素抖动.md',
+        content: require('./CSS过渡效果导致元素抖动.md').default.render()
+      },
+    ]
   },
 ];
 export default list
--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,11 @@ type Menu = {
   path: string;
   content: any;
 }
+type SubMenu = {
+  title: string;
+  menu: Menu[]
+}
+type MenuList = Array<Menu, SubMenu>
 interface anyObject<T = any> {
   [prop: string]: T
 }
